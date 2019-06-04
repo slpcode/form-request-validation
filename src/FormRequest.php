d3e6c9e5b5a520a9dc3ac26cdb00ee237c645b93
slpcode <1370808424@qq.com>
2019-06-04 18:39:03 +0800

    创建

---
+++ src/FormRequest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanbin
+ * Date: 2019/6/4
+ * Time: 15:33
+ */
+
+namespace Slpcode\FormRequestValidation;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class FormRequest extends Request
+{
+//    public function __construct()
+//    {
+//        parent::__construct($_GET, $_POST, [], $_COOKIE, $_FILES, $_SERVER);
+//    }
+}
